385d8dccce8a1d46ca6181aec8ac5bb4365120d2
murillol3039 <47570053+murillol3039@users.noreply.github.com>
2019-10-09 13:26:36 -0400

    Added M3LAB

---
+++ Adventure/Creepy house/M3LAB.py
@@ -0,0 +1,25 @@
+
+
+
+
+from rooms import Room
+
+kitchen = Room("Kitchen", "This is the kitchen.")
+livingRoom = Room("Living Room", "There's a dusty sofa here.")
+bedroom = Room("Bedroom", "There is a creepy doll here.")
+bathroom = Room("Bathroom", "There is blood in the tub")
+closet = Room("Closet", "There is a body in the closet")
+
+rooms = []
+rooms.append(kitchen)
+rooms.append(livingRoom)
+rooms.append(bedroom)
+rooms.append(bathroom)
+rooms.append(closet)
+
+for room in rooms:
+    print (room)
+              
+              
+               
+    
---
+++ Adventure/Creepy house/rooms.py
@@ -0,0 +1,24 @@
+# M3LAB
+# Lawrence Murillo
+# 10/07/2019
+
+
+
+
+# room.py - the Room class
+
+""" Definition of the Room class """
+class Room:
+    def __init__(self, name, description):
+        """ create a Room object """
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        """ human readable version of Room """
+        readable = "Room name: " + self.name
+        readable += "\n" # new line
+        readable += "Room description: " + self.description
+        
+        return readable
+    
